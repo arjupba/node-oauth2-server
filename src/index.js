5d3be44cd3b0dddbe3c5a0a699eb4911d6cad7ae
Arjun Gangadharan <arjungangadharan@wimoku.com>
2022-05-13 18:22:09 +0530

    feat: prettier config added

--- src/index.js
+++ src/index.js
@@ -1,29 +1,29 @@
-import "dotenv/config";
-import express from "express";
-import cors from "cors";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
 app.use(cors());
 
-console.log("test");
+console.log('test');
 
-app.get("/", (req, res) => {
-  return res.send("Received a GET HTTP method");
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
 });
 
-app.post("/", (req, res) => {
-  return res.send("Received a POST HTTP method");
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
 });
 
-app.put("/", (req, res) => {
-  return res.send("Received a PUT HTTP method");
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
 });
 
-app.delete("/", (req, res) => {
-  return res.send("Received a DELETE HTTP method");
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
 });
 
 app.listen(process.env.PORT, () =>
-  console.log(`Example app listening on port ${process.env.PORT}!`)
+  console.log(`Example app listening on port ${process.env.PORT}!`),
 );
